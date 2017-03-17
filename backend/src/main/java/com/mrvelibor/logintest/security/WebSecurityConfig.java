b89efa3bbd35cdc6d4a589ffae63f920530a81b6
mrvelibor <mr.velibor@gmail.com>
2017-03-17 16:06:32 +0100

    SAML

--- backend/src/main/java/com/mrvelibor/logintest/security/WebSecurityConfig.java
+++ backend/src/main/java/com/mrvelibor/logintest/security/WebSecurityConfig.java
@@ -465,7 +465,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     public MetadataGenerator metadataGenerator() {
         MetadataGenerator metadataGenerator = new MetadataGenerator();
-        metadataGenerator.setEntityId("com:vdenotaris:spring:sp");
+        metadataGenerator.setEntityId("com:mrvelibor:logintest:sp");
         metadataGenerator.setExtendedMetadata(extendedMetadata());
         metadataGenerator.setIncludeDiscoveryExtension(false);
         metadataGenerator.setKeyManager(keyManager()); 
