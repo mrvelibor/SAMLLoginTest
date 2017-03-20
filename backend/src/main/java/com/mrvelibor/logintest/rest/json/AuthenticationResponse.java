6070c675447846c9ba7b14b937d05f2b63303283
mrvelibor <mr.velibor@gmail.com>
2017-03-20 11:22:03 +0100

    Lombok

--- backend/src/main/java/com/mrvelibor/logintest/rest/json/AuthenticationResponse.java
+++ backend/src/main/java/com/mrvelibor/logintest/rest/json/AuthenticationResponse.java
@@ -1,43 +1,18 @@
 package com.mrvelibor.logintest.rest.json;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
 public class AuthenticationResponse {
 
+    @Getter @Setter
     private String token;
+    @Getter @Setter
     private String username;
+    @Getter @Setter
     private String type;
-
-    public AuthenticationResponse() {
-        super();
-    }
-
-    public AuthenticationResponse(String token, String username, String type) {
-        this.setToken(token);
-        this.setUsername(username);
-        this.setType(type);
-    }
-
-    public String getToken() {
-        return this.token;
-    }
-
-    public void setToken(String token) {
-        this.token = token;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
 
 }
